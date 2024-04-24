aebd8c0e20990b8c007dede4e21dfd78489617b5
helsaalika <helsa.alika.tif421@polban.ac.id>
2024-04-24 09:07:40 +0700

    tambah inputan

--- client.py
+++ client.py
@@ -1,7 +1,14 @@
 import rpyc
 
-conn = rpyc.connect("localhost", 12345)
+conn = rpyc.connect("192.168.13.90", 12345) # ip address
 
-# Submit a Covid report
-response = conn.root.submit_report("12345678901", "John Doe", "Jane Doe", "123 Main St", "Demam, Batuk")
-print("Response from server:", response)
+# Input data dari cmd
+nik = input("Masukkan NIK pelapor: ")
+nama_pelapor = input("Masukkan Nama Pelapor: ")
+nama_terduga = input("Masukkan Nama Terduga Covid: ")
+alamat_terduga = input("Masukkan Alamat Terduga Covid: ")
+gejala = input("Masukkan Gejala: ")
+
+# Submit laporan Covid
+response = conn.root.exposed_submit_report(nik, nama_pelapor, nama_terduga, alamat_terduga, gejala)
+print("Response dari server:", response)
